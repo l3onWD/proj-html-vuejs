c42b4286eb019d16db9796580658576867a7fc83
l3on <l3onwebdev@gmail.com>
2023-07-10 17:36:43 +0200

    added font awesome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import { faSearch, faAngleUp, faBagShopping, faUser } from '@fortawesome/free-solid-svg-icons';
+library.add(faSearch, faUser, faBagShopping, faAngleUp);
 
 createApp(App)
-    .mount('#app')
+    .component('FontAwesomeIcon', FontAwesomeIcon)
+    .mount('#app');

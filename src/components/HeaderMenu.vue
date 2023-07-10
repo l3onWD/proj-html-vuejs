8d0590feb4392f9fa5d2fbb9c6f710e399b5471b
l3on <l3onwebdev@gmail.com>
2023-07-10 16:45:17 +0200

    added header menu

---
+++ src/components/HeaderMenu.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+
+
+}
+</script>
+
+
+<template>
+    <ul class="d-flex justify-content-center">
+        <li><a href="#">Link 01</a></li>
+        <li><a href="#">Link 02</a></li>
+        <li><a href="#">Link 03</a></li>
+        <li><a href="#">Link 04</a></li>
+        <li><a href="#">Link 05</a></li>
+        <li><a href="#">Link 06</a></li>
+    </ul>
+</template>
+
+
+<style scoped>
+a {
+    padding: 1rem;
+    display: block;
+}
+</style>

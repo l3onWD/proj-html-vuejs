b65c85dce95a1f4439c312b0f5ba347b7a3d0aae
l3on <l3onwebdev@gmail.com>
2023-07-10 18:09:39 +0200

    added header actions style

--- src/components/header/HeaderActions.vue
+++ src/components/header/HeaderActions.vue
@@ -7,12 +7,35 @@ export default {
 
 
 <template>
-    <ul class="d-flex">
-        <li><button>icon</button></li>
-        <li><button>icon</button></li>
-        <li><button>icon</button></li>
+    <ul class="h-100 d-flex">
+
+        <!-- Search Products -->
+        <li>
+            <button>
+                <FontAwesomeIcon icon="fas fa-search" />
+            </button>
+        </li>
+
+        <!-- User Account -->
+        <li>
+            <button>
+                <FontAwesomeIcon icon="fas fa-user" />
+            </button>
+        </li>
+
+        <!-- Shopping Cart -->
+        <li>
+            <button>
+                <FontAwesomeIcon icon="fas fa-bag-shopping" /><span class="ms-2 text-yellow">(0) €0.00</span>
+            </button>
+        </li>
     </ul>
 </template>
 
 
-<style></style>
+<style scoped>
+button {
+    padding: 0.5rem;
+    color: #fff;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import App from './App.vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-import { faSearch, faAngleUp, faBagShopping, faUser } from '@fortawesome/free-solid-svg-icons';
-library.add(faSearch, faUser, faBagShopping, faAngleUp);
+import { faSearch, faAngleDown, faBagShopping, faUser } from '@fortawesome/free-solid-svg-icons';
+library.add(faSearch, faUser, faBagShopping, faAngleDown);
 
 createApp(App)
     .component('FontAwesomeIcon', FontAwesomeIcon)

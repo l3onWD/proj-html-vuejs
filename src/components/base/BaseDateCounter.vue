9ef8f1d3be3c1f779fcc1d49b9a71bc0edd7fc78
l3on <l3onwebdev@gmail.com>
2023-07-11 16:47:04 +0200

    added Date Counter component

---
+++ src/components/base/BaseDateCounter.vue
@@ -0,0 +1,66 @@
+<script>
+export default {
+    data() {
+        return {
+            timerId: null,
+            counter: 0
+        };
+    },
+
+    props: {
+        date: Number
+    },
+
+    computed: {
+        msLeft() {
+            return this.date - this.counter;
+        },
+
+        days() {
+            return Math.floor(this.msLeft / (1000 * 60 * 60 * 24));
+        },
+
+        hours() {
+            return Math.floor((this.msLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        },
+
+        minutes() {
+            return Math.floor((this.msLeft % (1000 * 60 * 60)) / (1000 * 60));
+        },
+
+        seconds() {
+            return Math.floor(this.msLeft % (1000 * 60) / 1000);
+        }
+    },
+
+    methods: {
+        updateCounter() {
+            this.counter += 1000;
+            if (this.counter >= this.date) {
+                this.counter = this.date;
+                clearInterval(this.timerId);
+            }
+        }
+    },
+
+    created() {
+        this.counter = Date.now();
+        if (this.counter >= this.date) this.counter = this.date;
+        else this.timerId = setInterval(this.updateCounter, 1000);
+    }
+
+}
+</script>
+
+
+<template>
+    <ul class="d-flex gap-3 justify-content-center py-3">
+        <li>{{ days }} days</li>
+        <li>{{ hours }} hours</li>
+        <li>{{ minutes }} mins</li>
+        <li>{{ seconds }} secs</li>
+    </ul>
+</template>
+
+
+<style></style>

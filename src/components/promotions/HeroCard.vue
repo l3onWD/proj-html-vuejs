ee09f6dbac000924f423654992d4346e5367f883
l3on <l3onwebdev@gmail.com>
2023-07-11 18:27:15 +0200

    added hero card component

---
+++ src/components/promotions/HeroCard.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+
+    props: {
+        imgFile: String,
+        imgTitle: String,
+    },
+    computed: {
+        imgPath() {
+            const url = new URL(`../../assets/img/banners/${this.imgFile}`, import.meta.url);
+            return url.href;
+        }
+    }
+}
+</script>
+
+
+<template>
+    <div class="hero-card">
+
+        <!-- Hero Image -->
+        <img :src="imgPath" :alt="imgTitle" class="hero-img">
+
+        <!-- Hero Content -->
+        <div class="hero-content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+.hero-card {
+    height: 100%;
+    position: relative;
+
+
+    .hero-img {
+        width: 100%;
+        height: 100%;
+
+        object-fit: cover;
+    }
+
+    .hero-content {
+        position: absolute;
+        inset: 0;
+
+        display: flex;
+        align-items: center;
+    }
+}
+</style>

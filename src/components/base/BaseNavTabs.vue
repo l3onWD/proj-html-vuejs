1ad157f0b86fc3a775069c325d3537ac634eb97e
l3on <l3onwebdev@gmail.com>
2023-07-11 15:56:28 +0200

    changed filter component name to base nav tabs

---
+++ src/components/base/BaseNavTabs.vue
@@ -0,0 +1,50 @@
+<script>
+
+export default {
+    data() {
+        return {
+            activeTab: null
+        };
+    },
+
+    props: {
+        tabList: Array
+    },
+
+    methods: {
+        onTabClicked(tabValue) {
+
+            this.activeTab = tabValue;
+            this.$emit('tab-changed', tabValue)
+        }
+    },
+
+    created() {
+        this.activeTab = this.tabList[0].value;
+    },
+
+    emits: ['tab-changed']
+
+}
+</script>
+
+
+<template>
+    <nav>
+        <ul class="d-flex gap-3 justify-content-center py-3">
+            <li v-for="tab in tabList" :key="tab.text">
+                <button @click="onTabClicked(tab.value)" :class="{ active: tab.value === activeTab }">{{ tab.text
+                }}</button>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+
+<style lang="scss" scoped>
+@use '@/assets/scss/vars' as *;
+
+button.active {
+    color: $col-yellow;
+}
+</style>

da7f9e6d0cd84175e53822263948dff39c7eb86f
l3on <l3onwebdev@gmail.com>
2023-07-11 12:15:04 +0200

    added blog data structure

---
+++ src/data/blog/_blogList.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        imgFile: 'Architecto-beatae-vitae-dicta.jpg',
+        title: 'Blog 01',
+        comments: 0,
+        content: 'Lorem ipsum',
+        published: '2023-01-25'
+    },
+    {
+        id: 2,
+        imgFile: 'At-vero-eos-et-accusamus.jpg',
+        title: 'Blog 02',
+        comments: 2,
+        content: 'Lorem ipsum',
+        published: '2023-01-15'
+    },
+    {
+        id: 3,
+        imgFile: 'Blandit-praesen-volupta.jpg',
+        title: 'Blog 03',
+        comments: 5,
+        content: 'Lorem ipsum',
+        published: '2022-11-07'
+    }
+];
--- src/data/index.js
+++ src/data/index.js
@@ -2,5 +2,7 @@ import headerMenuLinks from './_headerMenuLinks';
 import contactLinks from './_contactLinks';
 import productList from './products/_productList';
 import productDealsList from './products/_productDealsList';
+import blogList from './blog/_blogList';
 
-export { headerMenuLinks, contactLinks, productList, productDealsList };
+
+export { headerMenuLinks, contactLinks, productList, productDealsList, blogList };

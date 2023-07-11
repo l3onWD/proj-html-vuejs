c43bfba87b86e026cfe75b97d840ffb5a560c736
l3on <l3onwebdev@gmail.com>
2023-07-11 12:14:31 +0200

    imported blog icons from font awesome

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import App from './App.vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-import { faSearch, faAngleDown, faBagShopping, faUser, faStar } from '@fortawesome/free-solid-svg-icons';
-library.add(faSearch, faUser, faBagShopping, faAngleDown, faStar);
+import { faSearch, faAngleDown, faBagShopping, faUser, faStar, faCalendarDays, faComments } from '@fortawesome/free-solid-svg-icons';
+library.add(faSearch, faUser, faBagShopping, faAngleDown, faStar, faCalendarDays, faComments);
 
 createApp(App)
     .component('FontAwesomeIcon', FontAwesomeIcon)

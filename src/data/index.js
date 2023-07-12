3a0f7a43da6ffc06c4157ff09385c36197d1c705
l3on <l3onwebdev@gmail.com>
2023-07-12 17:46:01 +0200

    restructured index partials

--- src/data/index.js
+++ src/data/index.js
@@ -1,7 +1,11 @@
+/*** DATABASE ***/
+import productList from './db/products/_productList';
+import blogList from './db/blog/_blogList';
+
+
+/*** CONSTANTS ***/
 import headerMenuLinks from './_headerMenuLinks';
 import contactLinks from './_contactLinks';
-import productList from './products/_productList';
-import blogList from './blog/_blogList';
 import testimonialList from './_testimonialList';
 import footerMenues from './_footerMenues';
 import socialLinks from './_socialLinks';

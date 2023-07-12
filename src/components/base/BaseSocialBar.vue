33720dcd81b4c406d241aabe83aa3e936698e4ba
l3on <l3onwebdev@gmail.com>
2023-07-12 16:05:41 +0200

    added base social bar component

---
+++ src/components/base/BaseSocialBar.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+    props: {
+        socials: Array
+    }
+}
+</script>
+
+
+<template>
+    <nav class="social-bar py-2">
+        <ul>
+            <li v-for="social in socials" :key="social.link">
+                <a :href="social.link">
+                    <i>
+                        <FontAwesomeIcon :icon="['fab', social.iconClass]" />
+                    </i>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+
+<style lang="scss" scoped>
+nav {
+    ul {
+        display: flex;
+
+        a {
+            padding: 0.5rem;
+
+            display: block;
+
+            i {
+                width: 24px;
+                height: 24px;
+
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+        }
+    }
+}
+</style>

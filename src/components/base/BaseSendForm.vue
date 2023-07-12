892563d787a99ee312fae75ad60987609b9f1a3f
l3on <l3onwebdev@gmail.com>
2023-07-12 15:52:03 +0200

    added base send form component

---
+++ src/components/base/BaseSendForm.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+
+    props: {
+        iconClass: String
+    }
+}
+</script>
+
+
+<template>
+    <form>
+        <input type="text">
+        <button>
+            <FontAwesomeIcon :icon="['fas', iconClass]" size="lg" />
+        </button>
+    </form>
+</template>
+
+
+<style lang="scss" scoped>
+@use '@/assets/scss/vars' as *;
+
+
+form {
+    position: relative;
+
+    input {
+        padding-right: 40px;
+        width: 100%;
+    }
+
+    button {
+        position: absolute;
+        top: 50%;
+        right: 15px;
+        transform: translateY(-50%);
+
+        color: #000;
+
+        &:hover {
+            color: $col-yellow;
+        }
+    }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import App from './App.vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-import { faSearch, faAngleDown, faBagShopping, faUser, faStar, faCalendarDays, faComments, faTruck, faSackDollar, faPercent, faHandHoldingDollar, faCircleChevronLeft, faCircleChevronRight, faCircle, faHeart, faMaximize, faEye, faQuoteLeft, faPlus, faLink, faLocationDot, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
-library.add(faSearch, faUser, faBagShopping, faAngleDown, faStar, faCalendarDays, faComments, faTruck, faSackDollar, faPercent, faHandHoldingDollar, faCircleChevronLeft, faCircleChevronRight, faCircle, faHeart, faMaximize, faEye, faQuoteLeft, faPlus, faLink, faLocationDot, faPhone, faEnvelope);
+import { faSearch, faAngleDown, faBagShopping, faUser, faStar, faCalendarDays, faComments, faTruck, faSackDollar, faPercent, faHandHoldingDollar, faCircleChevronLeft, faCircleChevronRight, faCircle, faHeart, faMaximize, faEye, faQuoteLeft, faPlus, faLink, faLocationDot, faPhone, faEnvelope, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+library.add(faSearch, faUser, faBagShopping, faAngleDown, faStar, faCalendarDays, faComments, faTruck, faSackDollar, faPercent, faHandHoldingDollar, faCircleChevronLeft, faCircleChevronRight, faCircle, faHeart, faMaximize, faEye, faQuoteLeft, faPlus, faLink, faLocationDot, faPhone, faEnvelope, faPaperPlane);
 
 createApp(App)
     .component('FontAwesomeIcon', FontAwesomeIcon)

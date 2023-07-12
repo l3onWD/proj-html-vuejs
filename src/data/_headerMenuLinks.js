e1cc8c564c401bc82d29b0faac313ff271fb9aa1
l3on <l3onwebdev@gmail.com>
2023-07-12 16:16:14 +0200

    added submenu data structure

--- src/data/_headerMenuLinks.js
+++ src/data/_headerMenuLinks.js
@@ -1,8 +1,46 @@
 export default [
-    { url: '#', text: 'Home', isActive: true },
-    { url: '#', text: 'Shop', isActive: false },
-    { url: '#', text: 'Blog', isActive: false },
-    { url: '#', text: 'Media', isActive: false },
-    { url: '#', text: 'ShortCode', isActive: false },
-    { url: '#', text: 'Features', isActive: false }
+    {
+        url: '#',
+        text: 'Home',
+        isActive: true
+    },
+    {
+        url: '#',
+        text: 'Shop',
+        isActive: false
+    },
+    {
+        url: '#',
+        text: 'Blog',
+        isActive: false,
+        submenu: [
+            {
+                url: '#',
+                text: 'Sub 01'
+            },
+            {
+                url: '#',
+                text: 'Sub 02'
+            },
+            {
+                url: '#',
+                text: 'Sub 03'
+            }
+        ]
+    },
+    {
+        url: '#',
+        text: 'Media',
+        isActive: false
+    },
+    {
+        url: '#',
+        text: 'ShortCode',
+        isActive: false
+    },
+    {
+        url: '#',
+        text: 'Features',
+        isActive: false
+    }
 ]

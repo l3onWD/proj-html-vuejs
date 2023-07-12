db6a99f4080a4cc594894dcfc415d477ebf5e215
l3on <l3onwebdev@gmail.com>
2023-07-12 11:24:45 +0200

    changed carousel logic and style

--- src/components/base/BaseCarousel.vue
+++ src/components/base/BaseCarousel.vue
@@ -1,6 +1,16 @@
 <script>
 export default {
 
+    props: {
+        navDots: Boolean,
+        visibleSlides: Number
+    },
+
+    computed: {
+        carouselCols() {
+            return `row no-wrap row-cols-${this.visibleSlides} flex-nowrap`;
+        }
+    }
 
 }
 </script>
@@ -9,48 +19,51 @@ export default {
 <template>
     <div class="carousel">
 
-        <div class="carousel-items">
+        <!-- Slides Container -->
+        <div class="carousel-items" :class="carouselCols">
             <slot></slot>
         </div>
 
-        <button class="carousel-nav nav-prev">
-            <FontAwesomeIcon icon="fas fa-circle-chevron-left" size="2xl" />
-        </button>
-        <button class="carousel-nav nav-next">
-            <FontAwesomeIcon icon="fas fa-circle-chevron-right" size="2xl" />
-        </button>
+        <!-- Nav Arrows -->
+        <div v-if="!navDots">
+            <button class="nav-arrow nav-prev">
+                <FontAwesomeIcon icon="fas fa-circle-chevron-left" size="2xl" />
+            </button>
+            <button class="nav-arrow nav-next">
+                <FontAwesomeIcon icon="fas fa-circle-chevron-right" size="2xl" />
+            </button>
+        </div>
+
+        <!-- Nav Dots -->
+        <div v-else class="nav-dots">
+            <button v-for="n in 3" :key="n">
+                <FontAwesomeIcon icon="fas fa-circle" />
+            </button>
+        </div>
     </div>
 </template>
 
 
-<style lang="scss">
+<style lang="scss" scoped>
 @use '@/assets/scss/vars' as *;
 
 
 .carousel {
     position: relative;
 
+
+    overflow: hidden;
+
     .carousel-items {
-        padding: 0.25rem;
         position: relative;
 
         display: flex;
-
-        overflow: hidden;
-
-        &>* {
-            flex-basis: calc(100% / 4);
-        }
     }
 
-    .carousel-nav {
+    .nav-arrow {
         position: absolute;
         top: 50%;
         transform: translateY(-50%);
-
-        &:hover {
-            color: $col-yellow;
-        }
 
         &.nav-prev {
             left: 0;
@@ -58,6 +71,20 @@ export default {
 
         &.nav-next {
             right: 0;
+        }
+    }
+
+    .nav-dots {
+        padding: 1rem;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+
+        text-align: center;
+
+        &>button {
+            padding: 0.25rem;
         }
     }
 }

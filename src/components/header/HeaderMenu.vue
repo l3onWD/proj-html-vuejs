45c9406a5585b7aa37668341e3fc5d2df94bd8a8
l3on <l3onwebdev@gmail.com>
2023-07-15 13:31:29 +0200

    changed submenu hover logic

--- src/components/header/HeaderMenu.vue
+++ src/components/header/HeaderMenu.vue
@@ -2,16 +2,11 @@
 /* -----------------------------------------
 * RESOURCES
 -------------------------------------------*/
-/*** COMPONENTS ***/
-import BaseDropmenu from '@/components/base/BaseDropmenu.vue';
-
 /*** DATA ***/
 import { headerMenuLinks } from '@/data/';
 
 
 export default {
-    components: { BaseDropmenu },
-
     data() {
         return {
             links: headerMenuLinks
@@ -23,14 +18,18 @@ export default {
 
 
 <template>
-    <ul class="d-flex justify-content-center">
-        <li v-for="link in links" :key="link.text">
+    <ul class="header-menu">
+        <li v-for="link in links" :key="link.text" :class="{ 'position-relative': link.submenu }">
 
-            <a v-if="!link.submenu" :href="link.url" :class="{ active: link.isActive }">{{ link.text }}</a>
+            <a :href="link.url" :class="{ active: link.isActive }">{{ link.text
+            }}</a>
 
-            <BaseDropmenu v-else :links="link.submenu">
-                <a :href="link.url" :class="{ active: link.isActive }">{{ link.text }}</a>
-            </BaseDropmenu>
+            <ul v-if="link.submenu" class="header-submenu">
+                <li v-for="sublink in link.submenu" :key="sublink.text">
+                    <a :href="sublink.url" :class="{ active: sublink.isActive }">{{ sublink.text }}</a>
+                </li>
+            </ul>
+
         </li>
     </ul>
 </template>
@@ -40,15 +39,46 @@ export default {
 @use '@/assets/scss/vars' as *;
 
 
-a {
-    padding: 2rem 1.4rem;
+.header-menu {
+    display: flex;
+    justify-content: center;
 
-    display: block;
-    font-size: 1.1rem;
-    font-weight: bold;
+    a {
+        padding: 0.25rem 1rem;
 
-    &.active {
-        color: $col-yellow;
+        display: block;
+    }
+
+    &>li>a {
+        padding: 2rem 1.4rem;
+
+        display: block;
+        font-size: 1.1rem;
+        font-weight: bold;
+
+        &.active {
+            color: $col-yellow;
+        }
+    }
+
+    .header-submenu {
+        padding: 0.75rem 0;
+        position: absolute;
+        top: 100%;
+        left: 0;
+        min-width: 150px;
+
+        display: none;
+        background-color: $col-dark;
+        border: 1px solid $col-darkgray;
+
+        box-shadow: 0 0 8px 2px rgba($color: #000, $alpha: 0.2);
+        z-index: 1;
+
+    }
+
+    li:hover .header-submenu {
+        display: block;
     }
 }
 </style>

1cef2bb20e4247e46aad21d74a5873f0203bd8c0
l3on <l3onwebdev@gmail.com>
2023-07-29 18:24:23 +0200

    added basic header menu mobile style

--- src/components/header/HeaderMenu.vue
+++ src/components/header/HeaderMenu.vue
@@ -11,6 +11,10 @@ export default {
         return {
             links: headerMenuLinks
         };
+    },
+
+    props: {
+        showMobileMenu: Boolean
     }
 
 }
@@ -18,15 +22,15 @@ export default {
 
 
 <template>
-    <ul class="header-menu">
+    <ul class="header-menu" :class="{ 'show': showMobileMenu }">
         <li v-for="link in links" :key="link.text" :class="{ 'position-relative': link.submenu }">
 
-            <a :href="link.url" :class="{ active: link.isActive }">{{ link.text
+            <a :href="link.url" :class="{ active: link.showMobileMenu }">{{ link.text
             }}</a>
 
             <ul v-if="link.submenu" class="header-submenu">
                 <li v-for="sublink in link.submenu" :key="sublink.text">
-                    <a :href="sublink.url" :class="{ active: sublink.isActive }">{{ sublink.text }}</a>
+                    <a :href="sublink.url" :class="{ active: sublink.showMobileMenu }">{{ sublink.text }}</a>
                 </li>
             </ul>
 
@@ -40,45 +44,92 @@ export default {
 
 
 .header-menu {
-    display: flex;
-    justify-content: center;
+    position: fixed;
+    top: 0;
+    left: -200px;
+    width: 200px;
+    height: 100%;
+    padding: 2rem 0;
 
-    a {
-        padding: 0.25rem 1rem;
+    background-color: $col-dark;
+    visibility: hidden;
+    opacity: 0;
 
-        display: block;
+    z-index: 1;
+
+    &.show {
+        left: 0;
+
+        visibility: visible;
+        opacity: 1;
     }
 
-    &>li>a {
-        padding: 2rem 1.4rem;
+    a {
+        padding: 0.5rem 1.5rem;
 
         display: block;
-        font-size: 1.1rem;
-        font-weight: bold;
-
-        &.active {
-            color: $col-yellow;
-        }
     }
 
     .header-submenu {
-        padding: 0.75rem 0;
-        position: absolute;
-        top: 100%;
-        left: 0;
-        min-width: 150px;
+        padding-left: 1rem;
 
-        display: none;
-        background-color: $col-dark;
-        border: 1px solid $col-darkgray;
-
-        box-shadow: 0 0 8px 2px rgba($color: #000, $alpha: 0.2);
-        z-index: 1;
-
+        border-top: 1px solid $col-darkgray;
+        border-bottom: 1px solid $col-darkgray;
     }
 
-    li:hover .header-submenu {
-        display: block;
+}
+
+
+/* -----------------------------------------
+* RESPONSIVE
+-------------------------------------------*/
+/*** MEDIA LG ***/
+@media screen and (min-width: 992px) {
+    .header-menu {
+        position: static;
+        width: auto;
+        height: auto;
+        padding: 0;
+
+        display: flex;
+        justify-content: center;
+        visibility: visible;
+        opacity: 1;
+
+        a {
+            padding: 0.25rem 1rem;
+        }
+
+        &>li>a {
+            padding: 2rem 1.4rem;
+
+            display: block;
+            font-size: 1.1rem;
+            font-weight: bold;
+
+            &.active {
+                color: $col-yellow;
+            }
+        }
+
+        .header-submenu {
+            padding: 0.75rem 0;
+            position: absolute;
+            top: 100%;
+            left: 0;
+            min-width: 150px;
+
+            display: none;
+            background-color: $col-dark;
+            border: 1px solid $col-darkgray;
+
+            z-index: 1;
+
+        }
+
+        li:hover .header-submenu {
+            display: block;
+        }
     }
 }
 </style>

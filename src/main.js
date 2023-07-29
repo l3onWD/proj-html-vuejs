ad49ee118e93304689f2226604526504038f0313
l3on <l3onwebdev@gmail.com>
2023-07-29 12:12:52 +0200

    added click outside directive

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
+
+/*** FONT AWESOME DATA ***/
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
@@ -11,6 +13,27 @@ import { faFacebookF, faTwitter, faGooglePlusG, faSkype, faInstagram } from '@fo
 library.add(faSearch, faUser, faBagShopping, faAngleDown, faStar, faCalendarDays, faComments, faTruck, faSackDollar, faPercent, faHandHoldingDollar, faCircleChevronLeft, faCircleChevronRight, faCircle, faHeart, faMaximize, faEye, faQuoteLeft, faPlus, faLink, faLocationDot, faPhone, faEnvelope, faPaperPlane, faFacebookF, faTwitter, faGooglePlusG, faSkype, faInstagram);
 
 
+
+/*** INIT APP ***/
 createApp(App)
+
+    /*** CUSTOM DIRECTIVES ***/
+    .directive('click-outside', {
+        created(el, binding) {
+            el.clickOutsideEvent = e => {
+                if (!el.contains(e.target)) binding.value();
+            };
+
+            document.addEventListener('click', el.clickOutsideEvent);
+        },
+
+        unmounted(el) {
+            document.removeEventListener('click', el.clickOutsideEvent);
+        }
+    })
+
+    /*** GLOBAL COMPONENTS ***/
     .component('FontAwesomeIcon', FontAwesomeIcon)
+
+    /*** MOUNTING ***/
     .mount('#app');

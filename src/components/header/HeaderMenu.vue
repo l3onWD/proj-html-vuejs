5960aaefd324036c4b459e39abcd5d9c808de392
l3on <l3onwebdev@gmail.com>
2023-07-30 10:28:40 +0200

    added responsive submenu component

--- src/components/header/HeaderMenu.vue
+++ src/components/header/HeaderMenu.vue
@@ -2,16 +2,23 @@
 /* -----------------------------------------
 * RESOURCES
 -------------------------------------------*/
+/*** COMPONENTS ***/
+import HeaderSubmenu from '@/components/header/HeaderSubmenu.vue';
+
 /*** DATA ***/
 import { headerMenuLinks } from '@/data/';
 
 
 export default {
+    components: { HeaderSubmenu },
+
+
     data() {
         return {
             links: headerMenuLinks
         };
     },
+
 
     props: {
         showMobileMenu: Boolean
@@ -25,14 +32,9 @@ export default {
     <ul class="header-menu" :class="{ 'show': showMobileMenu }">
         <li v-for="link in links" :key="link.text" :class="{ 'position-relative': link.submenu }">
 
-            <a :href="link.url" :class="{ active: link.showMobileMenu }">{{ link.text
-            }}</a>
+            <a :href="link.url" :class="{ active: link.isActive }">{{ link.text }}</a>
 
-            <ul v-if="link.submenu" class="header-submenu">
-                <li v-for="sublink in link.submenu" :key="sublink.text">
-                    <a :href="sublink.url" :class="{ active: sublink.showMobileMenu }">{{ sublink.text }}</a>
-                </li>
-            </ul>
+            <HeaderSubmenu v-if="link.submenu" :links="link.submenu" />
 
         </li>
     </ul>
@@ -67,13 +69,10 @@ export default {
         padding: 0.5rem 1.5rem;
 
         display: block;
-    }
 
-    .header-submenu {
-        padding-left: 1rem;
-
-        border-top: 1px solid $col-darkgray;
-        border-bottom: 1px solid $col-darkgray;
+        &.active {
+            color: $col-yellow;
+        }
     }
 
 }
@@ -95,39 +94,12 @@ export default {
         visibility: visible;
         opacity: 1;
 
-        a {
-            padding: 0.25rem 1rem;
-        }
 
-        &>li>a {
+        a {
             padding: 2rem 1.4rem;
 
-            display: block;
             font-size: 1.1rem;
             font-weight: bold;
-
-            &.active {
-                color: $col-yellow;
-            }
-        }
-
-        .header-submenu {
-            padding: 0.75rem 0;
-            position: absolute;
-            top: 100%;
-            left: 0;
-            min-width: 150px;
-
-            display: none;
-            background-color: $col-dark;
-            border: 1px solid $col-darkgray;
-
-            z-index: 1;
-
-        }
-
-        li:hover .header-submenu {
-            display: block;
         }
     }
 }
---
+++ src/components/header/HeaderSubmenu.vue
@@ -0,0 +1,112 @@
+<script>
+export default {
+    data() {
+        return {
+            showMobileMenu: false
+        };
+    },
+
+    props: {
+        links: Array
+    }
+}
+</script>
+
+
+<template>
+    <button class="header-submenu-toggler" @click="showMobileMenu = !showMobileMenu">
+        <FontAwesomeIcon :icon="['fas', showMobileMenu ? 'angle-right' : 'angle-down']" />
+    </button>
+
+    <ul class="header-submenu" :class="{ 'show': showMobileMenu }">
+        <li v-for="link in links" :key="link.text">
+
+            <a :href="link.url" :class="{ active: link.isActive }">{{ link.text }}</a>
+
+        </li>
+    </ul>
+</template>
+
+
+<style lang="scss" scoped>
+@use '@/assets/scss/vars' as *;
+
+
+/*** SUBMENU ***/
+.header-submenu {
+    padding-left: 1rem;
+
+    display: none;
+    border-top: 1px solid $col-darkgray;
+    border-bottom: 1px solid $col-darkgray;
+
+    a {
+        padding: 0.5rem 1.5rem;
+
+        display: block;
+
+        &.active {
+            color: $col-yellow;
+        }
+    }
+}
+
+.header-submenu.show {
+    display: block;
+}
+
+
+/*** TOGGLER ***/
+.header-submenu-toggler {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 64px;
+    height: 32px;
+    padding: 0 0.5rem;
+
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+}
+
+/* -----------------------------------------
+* RESPONSIVE
+-------------------------------------------*/
+/*** MEDIA LG ***/
+@media screen and (min-width: 992px) {
+
+    /*** SUBMENU ***/
+    .header-submenu {
+        padding: 0.75rem 0;
+        position: absolute;
+        top: 100%;
+        left: 0;
+        min-width: 150px;
+
+        background-color: $col-dark;
+        border: 1px solid $col-darkgray;
+
+        z-index: 1;
+
+        a {
+            padding: 0.25rem 1rem;
+        }
+
+    }
+
+    .header-menu .header-submenu {
+        display: none;
+    }
+
+    li:hover>.header-submenu {
+        display: block;
+    }
+
+
+    /*** TOGGLER ***/
+    .header-submenu-toggler {
+        display: none;
+    }
+}
+</style>
